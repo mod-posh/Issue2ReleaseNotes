0cea63305dc8a0e0cd1e9ba33a8f83e2140e84ac
Jeff Patton <jeffrey@patton-tech.com>
2024-07-17 07:42:08 -0500

    Added utf-8 to handle properly encoding characters. Added user-agent. Simplified script logic and logging

--- issue2releasenotes.ps1
+++ issue2releasenotes.ps1
@@ -1,99 +1,104 @@
 param (
- [string]$MilestoneNumber
+  [string]$MilestoneNumber
 )
+
 try
 {
- $ErrorActionPreference = 'Stop';
- $Error.Clear();
+  $ErrorActionPreference = 'Stop';
+  $Error.Clear();
 
- $repository = $env:GITHUB_REPOSITORY
- $token = $env:GITHUB_TOKEN
- $verbose = $env:VERBOSE
- [System.Text.StringBuilder]$stringbuilder = [System.Text.StringBuilder]::new()
+  $repository = $env:GITHUB_REPOSITORY
+  $token = $env:GITHUB_TOKEN
+  $verbose = $env:VERBOSE
+  [System.Text.StringBuilder]$stringbuilder = [System.Text.StringBuilder]::new()
 
- $headers = @{
-  Authorization = "token $($token)"
- }
+  $headers = @{
+    Authorization = "token $token"
+    'User-Agent'  = 'PowerShell'
+  }
 
- $milestoneUri = "https://api.github.com/repos/$($repository)/milestones/$($MilestoneNumber)"
- $milestone = Invoke-RestMethod -Uri $milestoneUri -Headers $headers
+  $milestoneUri = "https://api.github.com/repos/$repository/milestones/$MilestoneNumber"
+  $milestone = Invoke-RestMethod -Uri $milestoneUri -Headers $headers
 
- if ($verbose.ToLower() -eq 'verbose')
- {
-  Write-Host "Issue2ReleaseNotes DEBUG"
-  Write-Host "Repository      : $($repository)"
-  Write-Host "MilestoneNumber : $($MilestoneNumber)"
-  Write-Host "MilestoneUri    : $($milestoneUri)"
- }
-
- if ($Milestone)
- {
-  $issuesUri = "https://api.github.com/repos/$($repository)/issues?state=closed&milestone=$($milestone.Number)"
-  $issues = Invoke-RestMethod -Uri $issuesUri -Headers $headers
-
-  if ($verbose.ToLower() -eq 'verbose')
+  if ($verbose -eq 'verbose')
   {
-   Write-Host "IssuesUri       : $($issuesUri)"
-   Write-Host "Issues          : $($issues.Count)"
+    Write-Host "Issue2ReleaseNotes DEBUG"
+    Write-Host "Repository      : $repository"
+    Write-Host "MilestoneNumber : $MilestoneNumber"
+    Write-Host "MilestoneUri    : $milestoneUri"
   }
 
-  $groupedIssues = @{}
-
-  foreach ($issue in $issues)
+  if ($milestone)
   {
-   $label = if ($issue.labels.Count -eq 0)
-   {
-    'No Label'
-   }
-   else
-   {
-    # Concatenate all label titles for an issue into a single string
-    ($issue.labels | ForEach-Object { $_.name }) -join ', '
-   }
+    $issuesUri = "https://api.github.com/repos/$repository/issues?state=closed&milestone=$($milestone.number)"
+    $issues = Invoke-RestMethod -Uri $issuesUri -Headers $headers
 
-   # Add the issue to the appropriate group in the hashtable
-   if (-not $groupedIssues.ContainsKey($label))
-   {
-    $groupedIssues[$label] = @()
-   }
-   $groupedIssues[$label] += $issue
+    if ($verbose -eq 'verbose')
+    {
+      Write-Host "IssuesUri       : $issuesUri"
+      Write-Host "Issues          : $($issues.Count)"
+    }
+
+    $groupedIssues = @{}
+
+    foreach ($issue in $issues)
+    {
+      $label = if ($issue.labels.Count -eq 0)
+      {
+        'No Label'
+      }
+      else
+      {
+        ($issue.labels | ForEach-Object { $_.name }) -join ', '
+      }
+
+      if (-not $groupedIssues.ContainsKey($label))
+      {
+        $groupedIssues[$label] = @()
+      }
+      $groupedIssues[$label] += $issue
+    }
+
+    $sortedKeys = $groupedIssues.Keys | Sort-Object -Property {
+      switch ($_)
+      {
+        'bug' { return 0 }
+        { $_ -like 'bug*' } { return 1 }
+        'No Label' { return 99 }
+        default { return 2 }
+      }
+    }
+
+    [void]$stringbuilder.AppendLine("# $($milestone.title)")
+    [void]$stringbuilder.AppendLine()
+
+    if ($milestone.description)
+    {
+      [void]$stringbuilder.AppendLine($milestone.description)
+    }
+
+    foreach ($key in $sortedKeys)
+    {
+      [void]$stringbuilder.AppendLine()
+      [void]$stringbuilder.AppendLine("## $($key.ToUpper())")
+      [void]$stringbuilder.AppendLine()
+
+      foreach ($issue in $groupedIssues[$key])
+      {
+        [void]$stringbuilder.AppendLine("* issue-$($issue.number): $($issue.title)")
+      }
+    }
+
+    if ($verbose -eq 'verbose')
+    {
+      $stringbuilder.ToString() | Write-Host
+    }
+
+    $stringbuilder.ToString() | Out-File RELEASE.md -Encoding utf8 -Force
   }
-
-  $sortedKeys = $groupedIssues.Keys | Sort-Object -Property {
-   if ($_ -eq 'bug') { return 0 }
-   if ($_ -like 'bug*') { return 1 }
-   elseif ($_ -eq 'No Label') { return 99 }
-   else { return 2 }
-  }
-  [void]$stringbuilder.AppendLine( "# $($milestone.title)" )
-  [void]$stringbuilder.AppendLine( "" )
-
-  if ($milestone.description)
-  {
-   [void]$stringbuilder.AppendLine( "$($milestone.description)" )
-  }
-
-  foreach ($key in $sortedKeys)
-  {
-   [void]$stringbuilder.AppendLine( "" )
-   [void]$stringbuilder.AppendLine( "## $($key.ToUpper())" )
-   [void]$stringbuilder.AppendLine( "" )
-
-   foreach ($issue in $groupedIssues[$key])
-   {
-    [void]$stringbuilder.AppendLine( "* issue-$($issue.number): $($issue.title)" )
-   }
-  }
-
- }
- if ($verbose.ToLower() -eq 'verbose')
- {
-  $stringbuilder.ToString();
- }
- $stringbuilder.ToString() |Out-File RELEASE.md -Encoding ascii -Force
 }
 catch
 {
- $_.InvocationInfo | Out-String;
- throw $_.Exception.Message;
+  $_.InvocationInfo | Out-String
+  throw $_.Exception.Message
 }
